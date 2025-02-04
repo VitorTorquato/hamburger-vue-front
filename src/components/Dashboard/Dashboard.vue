0fcc638e3c9e276ef04de73d6912bdbf7b2ad9a8
Vitor Torquato de Sousa <torquatovitor68@gmail.com>
2025-02-04 11:21:18 +0100

    style: dashboar styles

---
+++ src/components/Dashboard/Dashboard.vue
@@ -0,0 +1,103 @@
+<script>
+    export default{
+        name:'Dashboard'
+    }
+</script>
+
+<template>
+   <div id="burger-table">
+    <div>
+      <div id="burger-table-heading">
+        <div class="order-id">#:</div>
+        <div>Client:</div>
+        <div>Bread:</div>
+        <div>Meat:</div>
+        <div>Options:</div>
+        <div>Actions:</div>
+      </div>
+    </div>
+    <div id="burger-table-rows">
+      <div class="burger-table-row">
+        <div class="order-number">1</div>
+        <div>Joao</div>
+        <div>Integral</div>
+        <div>Picanha</div>
+        <div>
+          <ul>
+            <li>Salame</li>
+            <li>Bacons</li>
+          </ul>
+        </div>
+        <div>
+          <select name="status" class="status">
+            <option value="">
+                Select
+            </option>
+          </select>
+          <button class="delete-btn">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+#burger-table {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+
+  #burger-table-heading,
+  #burger-table-rows,
+  .burger-table-row {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  #burger-table-heading {
+    font-weight: bold;
+    padding: 12px;
+    border-bottom: 3px solid #333;
+  }
+
+  .burger-table-row {
+    width: 100%;
+    padding: 12px;
+    border-bottom: 1px solid #CCC;
+  }
+
+  #burger-table-heading div,
+  .burger-table-row div {
+    width: 19%;
+  }
+
+  #burger-table-heading .order-id,
+  .burger-table-row .order-number {
+    width: 5%;
+  }
+
+  select {
+    padding: 12px 6px;
+    margin-right: 12px;
+  }
+
+  .delete-btn {
+    background-color: #222;
+    color:#fcba03;
+    font-weight: bold;
+    border: 2px solid #222;
+    padding: 10px;
+    font-size: 16px;
+    margin: 0 auto;
+    cursor: pointer;
+    transition: .5s;
+  }
+  
+  .delete-btn:hover {
+    background-color: transparent;
+    color: #222;
+  }
+
+
+</style>
--- src/views/Order/OrderView.vue
+++ src/views/Order/OrderView.vue
@@ -1,7 +1,25 @@
-<script setup>
-
+<script>
+  import Dashboard from '@/components/Dashboard/Dashboard.vue';
+  export default{
+    components:{
+      Dashboard
+    },
+    name:'Orders'
+  }
 </script>
 
 <template>
-  <div class="main_container"><h1>Order</h1></div>
+  <div class="main_container">
+    <h1>Orders</h1>
+    <Dashboard />
+  </div>
 </template>
+
+<style scoped>
+  .main_container{
+    width: 100%;
+    max-width: 1400px;
+    min-height: calc(100dvh - 139px);
+    margin: 0 auto;
+  }
+</style>
